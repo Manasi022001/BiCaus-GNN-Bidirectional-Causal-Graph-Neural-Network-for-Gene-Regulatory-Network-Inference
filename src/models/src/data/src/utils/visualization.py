f7f50e27b63339e48c763d0450007559a0316eba
Manasi022001 <manasiphadke16@gmail.com>
2026-01-31 19:33:31 -0500

    Create visualization.py

---
+++ src/models/src/data/src/utils/visualization.py
@@ -0,0 +1,33 @@
+"""
+Visualization utilities
+"""
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+def plot_training_curves(history, save_path='training_curves.png'):
+    """
+    Plot training/validation curves
+    [COPY FROM NOTEBOOK: lines ~3440-3480]
+    """
+    pass
+
+def plot_predictions_scatter(predictions, targets, save_path='predictions.png'):
+    """
+    Scatter plot of predicted vs actual
+    """
+    pass
+
+def plot_attention_heatmap(attention_weights, chromatin_states, 
+                          save_path='attention_heatmap.png'):
+    """
+    Heatmap of attention by chromatin state
+    """
+    pass
+
+def plot_graph_statistics(data, corr_df, save_path='graph_stats.png'):
+    """
+    Visualize graph properties
+    [COPY FROM NOTEBOOK: lines ~2065-2110]
+    """
+    pass
